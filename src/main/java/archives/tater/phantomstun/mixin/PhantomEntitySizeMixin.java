96f5d7ba361975972df3507ce7bc75e0e0f233cf
ThePotatoArchivist <josiahfu@gmail.com>
2025-01-15 02:31:05 -0800

    wip sizing

---
+++ src/main/java/archives/tater/phantomstun/mixin/PhantomEntitySizeMixin.java
@@ -0,0 +1,53 @@
+package archives.tater.phantomstun.mixin;
+
+import net.minecraft.entity.EntityData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.PhantomEntity;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PhantomEntity.class)
+public abstract class PhantomEntitySizeMixin extends LivingEntity {
+    @Shadow public abstract int getPhantomSize();
+
+    protected PhantomEntitySizeMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @ModifyArg(
+            method = "initialize",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/PhantomEntity;setPhantomSize(I)V")
+    )
+    private int defaultSize(int size) {
+        return 1;
+    }
+
+    @Inject(
+            method = "initialize",
+            at = @At("TAIL")
+    )
+    private void heal(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, EntityData entityData, NbtCompound entityNbt, CallbackInfoReturnable<EntityData> cir) {
+        setHealth(getMaxHealth());
+    }
+
+    @Inject(
+            method = "onSizeChanged",
+            at = @At("TAIL")
+    )
+    private void setMaxHealth(CallbackInfo ci) {
+        this.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH).setBaseValue(20 + 10 * getPhantomSize());
+    }
+
+}
--- src/main/java/archives/tater/phantomstun/mixin/PhantomEntityStunMixin.java
+++ src/main/java/archives/tater/phantomstun/mixin/PhantomEntityStunMixin.java
@@ -17,11 +17,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PhantomEntity.class)
-public abstract class PhantomEntityMixin extends FlyingEntity implements Stunnable {
+public abstract class PhantomEntityStunMixin extends FlyingEntity implements Stunnable {
 	@Unique
 	private int phantomstun$stunnedTicks = 0;
 
-	protected PhantomEntityMixin(EntityType<? extends FlyingEntity> entityType, World world) {
+	protected PhantomEntityStunMixin(EntityType<? extends FlyingEntity> entityType, World world) {
 		super(entityType, world);
 	}
 
