84e507cf0805f5f72e9ee883644a01d8eccd943a
ThePotatoArchivist <josiahfu@gmail.com>
2025-10-01 00:24:40 -0700

    Port to 1.21.9

--- src/main/java/archives/tater/phantomstun/mixin/PhantomSpawnerTimingMixin.java
+++ src/main/java/archives/tater/phantomstun/mixin/PhantomSpawnerTimingMixin.java
@@ -2,9 +2,6 @@ package archives.tater.phantomstun.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.spawner.PhantomSpawner;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,6 +9,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.objectweb.asm.Opcodes;
+
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.spawner.PhantomSpawner;
 
 @Mixin(PhantomSpawner.class)
 @Debug(export = true)
@@ -33,7 +34,7 @@ public class PhantomSpawnerTimingMixin {
             method = "spawn",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/LocalDifficulty;getGlobalDifficulty()Lnet/minecraft/world/Difficulty;")
     )
-    private void setCooldown(ServerWorld world, boolean spawnMonsters, boolean spawnAnimals, CallbackInfo ci) {
+    private void setCooldown(ServerWorld world, boolean spawnMonsters, CallbackInfo ci) {
         // No more phantoms for 3-5 minutes
         if (cooldown < 180 * 20)
             cooldown = cooldown + (180 + world.random.nextInt(120)) * 20;
