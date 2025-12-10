9cedef89c4c7dad74c1c7ee376763f32b5b0f7b3
ThePotatoArchivist <josiahfu@gmail.com>
2025-12-10 01:46:44 -0800

    Port to 1.21.11

--- src/main/java/archives/tater/phantomstun/mixin/PhantomSpawnerHealthMixin.java
+++ src/main/java/archives/tater/phantomstun/mixin/PhantomSpawnerHealthMixin.java
@@ -2,12 +2,16 @@ package archives.tater.phantomstun.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.spawner.PhantomSpawner;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Slice;
+
+import org.objectweb.asm.Opcodes;
+
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.spawner.PhantomSpawner;
 
 @Mixin(PhantomSpawner.class)
 public class PhantomSpawnerHealthMixin {
@@ -15,10 +19,10 @@ public class PhantomSpawnerHealthMixin {
             method = "spawn",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I", ordinal = 0),
             slice = @Slice(
-                    from = @At(value = "FIELD", target = "Lnet/minecraft/stat/Stats;TIME_SINCE_REST:Lnet/minecraft/util/Identifier;")
+                    from = @At(value = "FIELD", target = "Lnet/minecraft/stat/Stats;TIME_SINCE_REST:Lnet/minecraft/util/Identifier;", opcode = Opcodes.GETSTATIC)
             )
     )
     private int checkHealth(int original, @Local(argsOnly = true) ServerWorld world, @Local ServerPlayerEntity player) {
-        return player.getHealth() < player.defaultMaxHealth / 2f ? 72000 : 0;
+        return player.getHealth() < player.getAttributeBaseValue(EntityAttributes.MAX_HEALTH) / 2f ? 72000 : 0;
    }
 }
--- src/main/java/archives/tater/phantomstun/mixin/PlayerEntityMixin.java
+++ src/main/java/archives/tater/phantomstun/mixin/PlayerEntityMixin.java
@@ -1,6 +1,12 @@
 package archives.tater.phantomstun.mixin;
 
 import archives.tater.phantomstun.Stunnable;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
@@ -8,11 +14,6 @@ import net.minecraft.entity.mob.PhantomEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Slice;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
@@ -20,29 +21,17 @@ public abstract class PlayerEntityMixin extends LivingEntity {
         super(entityType, world);
     }
 
-    @Inject(
+    @SuppressWarnings("ConstantValue")
+    @WrapOperation(
             method = "attack",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addCritParticles(Lnet/minecraft/entity/Entity;)V")
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addAttackParticlesAndSounds(Lnet/minecraft/entity/Entity;ZZZZF)V")
     )
-    private void stunPhantomCrit(Entity target, CallbackInfo ci) {
-        //noinspection ConstantValue
-        if (target instanceof PhantomEntity && (Object) this instanceof ServerPlayerEntity && !((Stunnable) target).phantomstun$isStunned()) {
-            ((Stunnable) target).phantomstun$setStunned();
-        }
-    }
+    private void stunPhantomCrit(PlayerEntity instance, Entity target, boolean criticalHit, boolean sweeping, boolean cooldownPassed, boolean pierce, float enchantDamage, Operation<Void> original) {
+        original.call(instance, target, criticalHit, sweeping, cooldownPassed, pierce, enchantDamage);
 
-    // a bit clunky but this playSound means it was a strong attack (cooldown > 0.9)
-    @Inject(
-            method = "attack",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playSound(Lnet/minecraft/entity/Entity;DDDLnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FF)V", ordinal = 0),
-            slice = @Slice(
-                    from = @At(value = "FIELD", target = "Lnet/minecraft/sound/SoundEvents;ENTITY_PLAYER_ATTACK_STRONG:Lnet/minecraft/sound/SoundEvent;")
-            )
-    )
-    private void stunPhantomAxe(Entity target, CallbackInfo ci) {
-        //noinspection ConstantValue
-        if (target instanceof PhantomEntity && (Object) this instanceof ServerPlayerEntity && !((Stunnable) target).phantomstun$isStunned() && this.getWeaponDisableBlockingForSeconds() > 0) {
-            ((Stunnable) target).phantomstun$setStunned();
-        }
+        if (!criticalHit && (!cooldownPassed || getWeaponDisableBlockingForSeconds() <= 0)) return;
+        if (!(target instanceof PhantomEntity || !((Object) this instanceof ServerPlayerEntity) || ((Stunnable) target).phantomstun$isStunned())) return;
+
+        ((Stunnable) target).phantomstun$setStunned();
     }
 }
