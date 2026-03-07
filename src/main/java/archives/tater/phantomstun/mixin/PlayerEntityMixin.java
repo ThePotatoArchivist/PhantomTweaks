4f30a90f893c3cbbd2c6df3617f817f12bad5de8
ThePotatoArchivist <josiahfu@gmail.com>
2026-03-06 20:57:12 -0800

    Fix boat crash I think
    Closes #8

--- src/main/java/archives/tater/phantomstun/mixin/PlayerEntityMixin.java
+++ src/main/java/archives/tater/phantomstun/mixin/PlayerEntityMixin.java
@@ -10,9 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.mob.PhantomEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.world.World;
 
 @Mixin(PlayerEntity.class)
@@ -21,7 +19,6 @@ public abstract class PlayerEntityMixin extends LivingEntity {
         super(entityType, world);
     }
 
-    @SuppressWarnings("ConstantValue")
     @WrapOperation(
             method = "attack",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addAttackParticlesAndSounds(Lnet/minecraft/entity/Entity;ZZZZF)V")
@@ -29,9 +26,10 @@ public abstract class PlayerEntityMixin extends LivingEntity {
     private void stunPhantomCrit(PlayerEntity instance, Entity target, boolean criticalHit, boolean sweeping, boolean cooldownPassed, boolean pierce, float enchantDamage, Operation<Void> original) {
         original.call(instance, target, criticalHit, sweeping, cooldownPassed, pierce, enchantDamage);
 
+        if (instance.getEntityWorld().isClient()) return;
         if (!criticalHit && (!cooldownPassed || getWeaponDisableBlockingForSeconds() <= 0)) return;
-        if (!(target instanceof PhantomEntity || !((Object) this instanceof ServerPlayerEntity) || ((Stunnable) target).phantomstun$isStunned())) return;
+        if (!(target instanceof Stunnable stunnable) || stunnable.phantomstun$isStunned()) return;
 
-        ((Stunnable) target).phantomstun$setStunned();
+        stunnable.phantomstun$setStunned();
     }
 }
