425de1d7130f7e1f8cbcd5c009c772884b4d01b2
ThePotatoArchivist <josiahfu@gmail.com>
2026-03-06 21:08:44 -0800

    Remap to mojmap

--- src/main/java/archives/tater/phantomstun/mixin/PhantomSpawnerHealthMixin.java
+++ src/main/java/archives/tater/phantomstun/mixin/PhantomSpawnerHealthMixin.java
@@ -2,27 +2,26 @@ package archives.tater.phantomstun.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.level.levelgen.PhantomSpawner;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Slice;
 
 import org.objectweb.asm.Opcodes;
 
-import net.minecraft.entity.attribute.EntityAttributes;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.spawner.PhantomSpawner;
-
 @Mixin(PhantomSpawner.class)
 public class PhantomSpawnerHealthMixin {
     @ModifyExpressionValue(
-            method = "spawn",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I", ordinal = 0),
+            method = "tick",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;nextInt(I)I", ordinal = 0),
             slice = @Slice(
-                    from = @At(value = "FIELD", target = "Lnet/minecraft/stat/Stats;TIME_SINCE_REST:Lnet/minecraft/util/Identifier;", opcode = Opcodes.GETSTATIC)
+                    from = @At(value = "FIELD", target = "Lnet/minecraft/stats/Stats;TIME_SINCE_REST:Lnet/minecraft/resources/Identifier;", opcode = Opcodes.GETSTATIC)
             )
     )
-    private int checkHealth(int original, @Local(argsOnly = true) ServerWorld world, @Local ServerPlayerEntity player) {
-        return player.getHealth() < player.getAttributeBaseValue(EntityAttributes.MAX_HEALTH) / 2f ? 72000 : 0;
+    private int checkHealth(int original, @Local(argsOnly = true) ServerLevel world, @Local ServerPlayer player) {
+        return player.getHealth() < player.getAttributeBaseValue(Attributes.MAX_HEALTH) / 2f ? 72000 : 0;
    }
 }
--- src/main/java/archives/tater/phantomstun/mixin/PhantomSpawnerTimingMixin.java
+++ src/main/java/archives/tater/phantomstun/mixin/PhantomSpawnerTimingMixin.java
@@ -2,6 +2,8 @@ package archives.tater.phantomstun.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.levelgen.PhantomSpawner;
 import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -11,32 +13,29 @@ import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.objectweb.asm.Opcodes;
 
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.world.spawner.PhantomSpawner;
-
 @Mixin(PhantomSpawner.class)
 @Debug(export = true)
 public class PhantomSpawnerTimingMixin {
-    @Shadow private int cooldown;
+    @Shadow private int nextTick;
 
     @WrapOperation(
-            method = "spawn",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/world/spawner/PhantomSpawner;cooldown:I", opcode = Opcodes.PUTFIELD),
+            method = "tick",
+            at = @At(value = "FIELD", target = "Lnet/minecraft/world/level/levelgen/PhantomSpawner;nextTick:I", opcode = Opcodes.PUTFIELD),
             slice = @Slice(
-                    from = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I", ordinal = 0)
+                    from = @At(value = "INVOKE", target = "Lnet/minecraft/util/RandomSource;nextInt(I)I", ordinal = 0)
             )
     )
     private void changeCooldown(PhantomSpawner instance, int value, Operation<Void> original) {
-        original.call(instance, cooldown + 15 * 20);
+        original.call(instance, nextTick + 15 * 20);
     }
 
     @Inject(
-            method = "spawn",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/LocalDifficulty;getGlobalDifficulty()Lnet/minecraft/world/Difficulty;")
+            method = "tick",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/DifficultyInstance;getDifficulty()Lnet/minecraft/world/Difficulty;")
     )
-    private void setCooldown(ServerWorld world, boolean spawnMonsters, CallbackInfo ci) {
+    private void setCooldown(ServerLevel world, boolean spawnMonsters, CallbackInfo ci) {
         // No more phantoms for 3-5 minutes
-        if (cooldown < 180 * 20)
-            cooldown = cooldown + (180 + world.random.nextInt(120)) * 20;
+        if (nextTick < 180 * 20)
+            nextTick = nextTick + (180 + world.random.nextInt(120)) * 20;
     }
 }
